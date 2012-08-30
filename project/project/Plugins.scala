98f1018b35e87d4e6aaa7f4c57013b3c61a6a94a
Peter Brant <peter.brant@gmail.com>
2012-08-30 23:52:05 +0200

    Update plugin location

--- project/project/Plugins.scala
+++ project/project/Plugins.scala
@@ -2,5 +2,5 @@ import sbt._
 
 object PluginDef extends Build {
   lazy val root              = Project("plugins", file(".")) dependsOn(buildPlugin)
-  lazy val buildPlugin       = uri("git://github.com/indrajitr/sbt-lift-build-plugin.git#c78f617f62")
+  lazy val buildPlugin       = uri("git://github.com/lift/sbt-lift-build.git#c78f617f62")
 }
